78391922eac8d6506f1b9863df831c3d92f2ab51
Thomas Knickerbocker <tcknickerbocker@gmail.com>
2025-06-30 16:59:43 -0400

    Displays planets and films. planet/Films endpoint appears to be broken and styling is yet to be added

--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import PlanetsPage from "./Components/Planets/PlanetsPage.jsx";
+import NotFound from "./Components/NotFound.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route exact path="/" element={<App />} />
+        <Route path="/planet/:id" element={<PlanetsPage />} />
+        <Route path="*" element={<NotFound />} /> {/* Default route for undefined paths */}
       </Routes>
     </Router>
   </StrictMode>
